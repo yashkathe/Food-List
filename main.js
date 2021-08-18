fa7fd87b7df79f5790cc044a5c056ae2ba8b2cbc
Yash Kathe <yashkathe123@gmail.com>
2021-08-18 23:36:40 +0530

    toggle info started and appending lists conditionally complete

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 //getting form to user onSUbmit and list to append
 
 const form = document.querySelector("form");
-const FoodList = document.getElementById("list");
+const breakFastLi = document.getElementById("breakFastLi");
+const lunchLi = document.getElementById("lunchLi");
+const snackLi = document.getElementById("snackLi");
+const dinnerLi = document.getElementById("dinnerLi");
 
 //getting user input
 
 const nameOfFood = document.querySelector("[name='food']");
 const typeOfFood = document.getElementById("select-opt");
+
+//getting buttons and p for toggle
+
+const info_btn = document.getElementById("info_btn")
+const info_para = document.getElementById("info_para")
 
 //empty arrays
 
@@ -21,6 +29,13 @@ function errorCheck() {
         alert("Dont leave the field empty");
         return;
     }
+}
+
+//creating a new list item to display
+function addElement(foodName , listname){
+    const li = document.createElement('li')
+    li.innerHTML = foodName
+    // listname.appendChild(li)
 }
 
 //this function will fire up upon form submission
@@ -43,15 +58,49 @@ form.onsubmit = (event) => {
     //conditionally entering values
     if (typeOfFood.value === "Breakfast") {
         breakfastList.push(newFood);
+        const li = document.createElement('li')
+        li.innerHTML = newFood.name
+        breakFastLi.appendChild(li)
     } else if (typeOfFood.value === "Lunch") {
         lunchList.push(newFood);
+        const li = document.createElement('li')
+        li.innerHTML = newFood.name
+        lunchLi.appendChild(li)
     } else if (typeOfFood.value === "Snack") {
         snackList.push(newFood);
+        const li = document.createElement('li')
+        li.innerHTML = newFood.name
+        snackLi.appendChild(li)
     } else if (typeOfFood.value === "Dinner") {
         snackList.push(newFood);
+        const li = document.createElement('li')
+        li.innerHTML = newFood.name
+        dinnerLi.appendChild(li)
     } else {
         alert('enter a food type')
     }
 
-    console.log(breakfastList);
+    // console.log(breakfastList);
+    // console.log(lunchList)
+    // console.log(snackList)
+    // console.log(dinnerList)
 };
+
+//toggling the info button
+
+function toggleInfo(){
+    function togglei(){
+        info_btn.classList.toggle('info__circle_visible')
+    }
+
+    function togglep(){
+        info_para.classList.toggle('toggle_info_visible')
+    }
+
+    info_btn.addEventListener('click', togglei)
+    info_btn.addEventListener('click', togglep)
+    
+}
+
+toggleInfo()
+
